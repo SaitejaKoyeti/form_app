b3c2b8d962c349e08419e47bd3186a9c534595d7
Saitejakoyeti <saitejakoyeti9@gmail.com>
2023-10-19 10:02:27 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( MyApp());
+}
+
+class MyApp extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => _My {
+
+  }
+
+}
